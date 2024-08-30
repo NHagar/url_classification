3c50198f3c5377f707cd1c0c47d8325a817fedd8
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-08-30 15:25:43 -0500

    args for pipeline steps

--- url_classification/model_training/train.py
+++ url_classification/model_training/train.py
@@ -6,6 +6,9 @@ from url_classification.model_training.models import train_bert_clf, train_dista
 # command line arguments for selecting which dataset to train on
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, required=True, choices=["huffpo", "uci", "recognasumm"])
+parser.add_argument("--train_bert", action="store_true", help="Train BERT model")
+parser.add_argument("--train_distant", action="store_true", help="Train distant labeler")
+parser.add_argument("--train_xgboost", action="store_true", help="Train XGBoost model")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -13,17 +16,20 @@ if __name__ == "__main__":
     train, val, test = make_splits(df)
 
     # train BERT model
-    if args.dataset == "recongasumm":
-        bert_model_name = "distilbert-base-multilingual-cased"
-    else:
-        bert_model_name = "distilbert-base-uncased"
+    if args.train_bert:
+        if args.dataset == "recongasumm":
+            bert_model_name = "distilbert-base-multilingual-cased"
+        else:
+            bert_model_name = "distilbert-base-uncased"
 
-    train_bert_clf(train.x, train.y, val.x, val.y, model_name=bert_model_name, output_name=args.dataset)
+        train_bert_clf(train.x, train.y, val.x, val.y, model_name=bert_model_name, output_name=args.dataset)
 
     # train distant labeler
-    train_distant_labeler(train, mapping_file=mapping_fpath, output_name=args.dataset)
+    if args.train_distant:
+        train_distant_labeler(train, mapping_file=mapping_fpath, output_name=args.dataset)
 
     # train xgboost model
-    train_xgboost(train["text"], train["y"], output_name=args.dataset)
+    if args.train_xgboost:
+        train_xgboost(train["text"], train["y"], output_name=args.dataset)
 
     test.to_csv(f"data/processed/{args.dataset}_test.csv", index=False)
