228c633bd3ce01aa3e89d7068be862aec4cbb129
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-09 14:58:40 -0500

    new cl args

--- url_classification/model_training/train.py
+++ url_classification/model_training/train.py
@@ -1,14 +1,34 @@
 import argparse
 
 from url_classification.data_processing.datasets import load_data, make_splits
-from url_classification.model_training.models import train_bert_clf, train_distant_labeler, train_xgboost
+from url_classification.model_training.models import (
+    train_bert_clf,
+    train_distant_labeler,
+    train_xgboost,
+)
 
 # command line arguments for selecting which dataset to train on
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str, required=True, choices=["huffpo", "uci", "recognasumm"])
+parser.add_argument(
+    "--dataset", type=str, required=True, choices=["huffpo", "uci", "recognasumm"]
+)
 parser.add_argument("--train_bert", action="store_true", help="Train BERT model")
-parser.add_argument("--train_distant", action="store_true", help="Train distant labeler")
+parser.add_argument(
+    "--train_distant", action="store_true", help="Train distant labeler"
+)
 parser.add_argument("--train_xgboost", action="store_true", help="Train XGBoost model")
+parser.add_argument(
+    "--bert_variant",
+    type=str,
+    choices=["a", "b", "c"],
+    help="Optional text variants for testing BERT",
+)
+parser.add_argument(
+    "--bert_url",
+    type=str,
+    choices=["path", "netloc_path"],
+    help="Optional URL variants for testing BERT",
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -22,11 +42,20 @@ if __name__ == "__main__":
         else:
             bert_model_name = "distilbert-base-uncased"
 
-        train_bert_clf(train.x, train.y, val.x, val.y, model_name=bert_model_name, output_name=args.dataset)
+        train_bert_clf(
+            train.x,
+            train.y,
+            val.x,
+            val.y,
+            model_name=bert_model_name,
+            output_name=args.dataset,
+        )
 
     # train distant labeler
     if args.train_distant:
-        train_distant_labeler(train, mapping_file=mapping_fpath, output_name=args.dataset)
+        train_distant_labeler(
+            train, mapping_file=mapping_fpath, output_name=args.dataset
+        )
 
     # train xgboost model
     if args.train_xgboost:
