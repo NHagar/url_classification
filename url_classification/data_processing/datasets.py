6bc076c62807159da7873191f2ef794df27742d3
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-09 14:41:27 -0500

    update queries for additional conditions

--- url_classification/data_processing/datasets.py
+++ url_classification/data_processing/datasets.py
@@ -21,7 +21,17 @@ SELECT category, COUNT(*) AS articles FROM data GROUP BY 1
 pcts AS (
 SELECT category, articles / total.total AS pct FROM counts, total
 )
-SELECT d.*, d.headline || ' ' || d.short_description AS text, category AS y FROM data d JOIN pcts USING (category) WHERE pct > 0.02
+SELECT
+    d.*,
+    d.headline || ' ' || d.short_description AS text,
+    d.headline AS bert_a,
+    d.short_description AS bert_b,
+    d.URL || ' ' || d.headline || ' ' || d.short_description AS bert_c,
+    category AS y
+FROM
+    data d JOIN pcts
+USING (category)
+WHERE pct > 0.02
 """,
     "uci": """
 SELECT *, TITLE AS text, CATEGORY AS y FROM 'data/raw/uci_categories.parquet'
@@ -42,27 +52,39 @@ SELECT Categoria AS category, COUNT(*) AS articles FROM data GROUP BY 1
 pcts AS (
 SELECT category, articles / total.total AS pct FROM counts, total
 )
-SELECT d.*, d.Titulo || ' ' || d.Subtitulo AS text, category AS y FROM data d JOIN pcts ON d.Categoria = pcts.category WHERE pct > 0.02 AND d.URL LIKE 'http%'
-"""
+SELECT
+    d.*,
+    d.Titulo || ' ' || d.Subtitulo AS text,
+    d.Titulo AS bert_a,
+    d.Sumario AS bert_b,
+    d.URL || ' ' || d.Titulo || ' ' || d.Sumario AS bert_c,
+    category AS y
+FROM
+    data d JOIN pcts
+ON d.Categoria = pcts.category
+WHERE pct > 0.02 AND d.URL LIKE 'http%'
+""",
 }
 
 MAPPING_PATHS = {
     "huffpo": None,
     "uci": "data/uci_categories_attributed.csv",
-    "recognasumm": "data/recognasumm_categories_attributed.csv"
+    "recognasumm": "data/recognasumm_categories_attributed.csv",
 }
 
+
 def load_data(query_name):
-    con = duckdb.connect(database=':memory:', read_only=False)
+    con = duckdb.connect(database=":memory:", read_only=False)
 
     q = QUERIES[query_name]
 
     data = con.execute(q).fetch_df()
     data["x"] = data.URL.apply(lambda x: urlparse(x).path)
-    data["x"] = data.x.str.replace(r'[/\-\\]', ' ', regex=True)
-    data = data[data.x.str.contains(r'\w', regex=True)]
+    data["x"] = data.x.str.replace(r"[/\-\\]", " ", regex=True)
+    data = data[data.x.str.contains(r"\w", regex=True)]
 
     return data, MAPPING_PATHS[query_name]
+
 
 def make_splits(df, seed=20240823):
     train, test = train_test_split(df, test_size=0.2, random_state=seed)
