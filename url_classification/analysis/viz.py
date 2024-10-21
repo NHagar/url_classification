c6bf6f7a18da234b11b582fffd98a953e70b6956
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-21 14:37:51 -0500

    visualization

---
+++ url_classification/analysis/viz.py
@@ -0,0 +1,21 @@
+import duckdb
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+con = duckdb.connect(database=":memory:", read_only=False)
+df = con.execute(
+    "SELECT model, dataset, throughput FROM './data/processed/evaluation_metrics_with_variants_throughput.csv' WHERE text_variant IS NULL AND url_variant IS NULL"
+).fetchdf()
+
+p = sns.barplot(data=df, x="model", y="throughput")
+
+plt.yscale("log")
+
+plt.xlabel("Model")
+plt.ylabel("Throughput (log scale)")
+
+
+# save plot
+plt.savefig("./data/processed/throughput.png")
+
+plt.show()
