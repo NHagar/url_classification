e176db08fc232639c8add5a031abcf1a8cd3d291
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-06-16 17:45:08 -0500

    short walltime

--- short.sh
+++ short.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each computer
-#SBATCH --time=12:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=2G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=train-short  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --mail-user=nicholas.hagar@northwestern.edu
