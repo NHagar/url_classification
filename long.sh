ed4b57b45fda816fc7a8b72e007378ada6dbe0af
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-06-16 17:44:00 -0500

    slurm scripts

---
+++ long.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=16 ## how many cpus or processors do you need on each computer
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --mem-per-cpu=2G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=train-long  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --mail-user=nicholas.hagar@northwestern.edu
+#SBATCH --mail-type=ALL
+
+module purge all
+
+uv run -m url_classification.train_and_evaluate --models gradient-boosting --mode train
