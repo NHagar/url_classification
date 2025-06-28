e7df236e8a27bbd23d7d6e9f6f50bebe29277027
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-06-28 16:27:12 -0500

    move jobs for output

--- short.sh
+++ short.sh
@@ -11,4 +11,4 @@
 
 module purge all
 
-uv run -m url_classification.train_and_evaluate --models log-reg svm tree-ensemble distant-labeling --mode evaluate
+uv run -m url_classification.train_and_evaluate --models log-reg svm tree-ensemble distant-labeling gradient-boosting --mode evaluate
