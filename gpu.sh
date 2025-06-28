f26c0a6f78a2d9a7db04ec25ea7c663b0c68d8d6
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-06-28 15:31:58 -0500

    switch scripts to eval

--- gpu.sh
+++ gpu.sh
@@ -6,10 +6,10 @@
 #SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each computer
 #SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=2G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=train-gpu  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --job-name=evaluate-gpu  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --mail-user=nicholas.hagar@northwestern.edu
 #SBATCH --mail-type=ALL
 
 module purge all
 
-uv run -m url_classification.train_and_evaluate --models distilbert distilbert-3k distilbert-1k xgboost --mode train
+uv run -m url_classification.train_and_evaluate --models distilbert distilbert-3k distilbert-1k xgboost --mode evaluate
--- short.sh
+++ short.sh
@@ -5,10 +5,10 @@
 #SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each computer
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=2G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=train-short  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --job-name=evaluate-short  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --mail-user=nicholas.hagar@northwestern.edu
 #SBATCH --mail-type=ALL
 
 module purge all
 
-uv run -m url_classification.train_and_evaluate --models log-reg svm tree-ensemble distant-labeling --mode train
+uv run -m url_classification.train_and_evaluate --models log-reg svm tree-ensemble distant-labeling --mode evaluate
